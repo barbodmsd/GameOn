9b96c2ffe259d3cf4a14a2b9364f942e38254809
Barbodmsd <barbodmasoudi@gmail.com>
2024-07-19 13:49:41 +0430

    install react tilt

---
+++ client/app/games/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Games() {
+  return <div className='h-screen px-8 mt-5'>Games</div>;
+}
+
+export const generateMetadata=()=>{
+    return {
+        title:'Games'
+    }
+}
