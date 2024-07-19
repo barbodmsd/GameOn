747b6bd61072ae123ad5c49fbb6941a80383b674
Barbodmsd <barbodmasoudi@gmail.com>
2024-07-19 12:31:09 +0430

    write theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "my-yellow": "#bdfd00",
+        'txt': "#7d8085",
+        "s-txt": {
+          '100': "#191919",
+          '200': "#212123",
+          '300': "#28282a",
+        },
+      },
     },
   },
   plugins: [],
