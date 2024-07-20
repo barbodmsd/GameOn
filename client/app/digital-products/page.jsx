6ee9067aa5a195b57f891d8f959da0b89581e632
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-07-20 12:26:47 -0700

    Update page.jsx

--- client/app/digital-products/page.jsx
+++ client/app/digital-products/page.jsx
@@ -17,15 +17,15 @@ export default function DigitalProducts() {
     })()
   },[])
   return (
-    <div className="h-screen px-8 mt-5 flex justify-between gap-5">
-      <div className="w-full max-h-[300px] bg-[#191919] rounded-2xl flex justify-around  ">
-        <div>
-        <h1>{banner?.title}</h1>
-        <p>{banner?.description}</p>
-        <button type="button">Get The Game</button>
+    <div className="h-screen px-8 mt-5 flex justify-between gap-5 ">
+      <div className="w-full max-h-[300px] bg-[#191919] rounded-2xl flex justify-around items-center ">
+        <div className="w-[370px] h-[200px] flex  flex-col justify-center items-center gap-5">
+        <h1 className=" text-3xl">{banner?.title}</h1>
+        <p className=" text-xs">{banner?.description}</p>
+        <button type="button" className=" bg-[#BDFD00] p-2 rounded-2xl text-black font-bold" >Get The Game</button>
         </div>
-        <div>
-          <img src={"http://localhost:7000/"+banner?.image} alt="baner-image" className="w-" />
+        <div className="w-[55%] -translate-y-1 h-[115%]">
+          <img src={"http://localhost:7000/"+banner?.image} alt="baner-image" className=" w-[90%] h-[90%]" />
         </div>
       </div>
       <div className="w-[230px] h-[530px] bg-black rounded-l-2xl">
