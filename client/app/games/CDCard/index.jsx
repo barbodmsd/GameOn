33ca2656860de5e44b06790cd7244fea884a94a5
Barbodmsd <barbodmasoudi@gmail.com>
2024-07-20 15:51:32 +0430

    update CDcard

--- client/app/games/CDCard/index.jsx
+++ client/app/games/CDCard/index.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-
+import Tilt from 'react-parallax-tilt'
 export default function CDCard({ title, img, id, price, platform }) {
   return (
-    <>
+    <Tilt>
       <div
-        className='w-[250px] h-[300px] rounded-md relative flex flex-col gap-3'
+        className='w-[250px] min-h-[90px] overflow-hidden rounded-md relative flex flex-col gap-3'
         >
-        <img width={"100%"} height={"100%"} src={img} alt={title} />
-        <span className='absolute top-1 left-1 bg-gray-800 p-1 rounded text-my-yellow'>
+        <img width={"100%"} className="rounded-md" height={"100%"} src={img} alt={title} />
+        <span className='absolute top-1 left-1 bg-gray-600  rounded text-my-yellow'>
           ${price}
         </span>
+        <div className="flex justify-between w-[100%] items-center">
+            <div>
+                <h6>{title.slice(0,5)}</h6>
+                <p className="text-txt">{platform.slice(0,5)}</p>
+            </div>
+            <div  className=" border rounded-full px-1.5 text-my-yellow border-my-yellow flex justify-center ">+</div>
+        </div>
       </div>
-    </>
+    </Tilt>
   );
 }
--- client/app/games/Products/index.jsx
+++ client/app/games/Products/index.jsx
@@ -11,16 +11,20 @@ export default function Products() {
       try {
         const res = await fetch("http://localhost:7000/products");
         const data = await res.json();
-        setProducts(data.data);
+        setProducts(data.data.slice(4,10));
       } catch (error) {
         console.log(error);
       }
     })();
   }, []);
   const items = products?.map((e, index) => (
-    <CDCard key={index} title={e.title} img={process.env.API_URL + e.images[1]} price={e.price} />
+    <CDCard
+      key={index}
+      platform={e.platform}
+      title={e.title}
+      img={"http://localhost:7000/" + e.images[1]}
+      price={e.price}
+    />
   ));
-  return <div className='flex flex-wrap w-[100%]'>{
-    items
-  }</div>;
+  return <div className='flex gap-3 flex-wrap w-[100%]'>{items}</div>;
 }
