69f9a0c75cc9801c3b94712b8e4f0382ef8de627
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-07-20 15:30:56 -0700

    Update page.jsx

--- client/app/digital-products/page.jsx
+++ client/app/digital-products/page.jsx
@@ -1,35 +1,68 @@
-"use client"
-
+"use client";
 import fetchData from "@/Utils/FetchData";
 import { useEffect, useState } from "react";
+import Card from "./Card";
+import Products from "./Products";
 
 export default function DigitalProducts() {
-  const [banner,setBanner] = useState()
-  console.log(banner)
-  useEffect(()=>{
-    (async()=>{
+  const [banner, setBanner] = useState();
+  const [productsCard, setProductsCard] = useState();
+  console.log(productsCard);
+  useEffect(() => {
+    (async () => {
       try {
-        const res = await fetchData("banners")
-        setBanner(res.data[0])
+        const res = await fetchData("banners");
+        setBanner(res.data[0]);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    })()
-  },[])
+    })();
+  }, []);
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetchData("products");
+        setProductsCard(res.data.slice(0, 4));
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+  const cardDtyle = productsCard?.map((e, index) => (
+    <Card
+      key={index}
+      title={e.title}
+      brand={e.brand}
+      price={e.price}
+      image={"http://localhost:7000/" + e.images[1]}
+    />
+  ));
   return (
-    <div className="h-screen px-8 mt-5 flex justify-between gap-5 ">
-      <div className="w-full max-h-[300px] bg-[#191919] rounded-2xl flex justify-around items-center ">
-        <div className="w-[370px] h-[200px] flex  flex-col justify-center items-center gap-5">
-        <h1 className=" text-3xl">{banner?.title}</h1>
-        <p className=" text-xs">{banner?.description}</p>
-        <button type="button" className=" bg-[#BDFD00] p-2 rounded-2xl text-black font-bold" >Get The Game</button>
+    <div className="px-8 mt-5">
+      <div className=" h-[300px] flex justify-between gap-5 ">
+        <div className="w-full  bg-[#191919] rounded-2xl flex justify-around items-center ">
+          <div className="w-[350px] h-[200px] flex  flex-col justify-center items-center text-center gap-5">
+            <h1 className=" text-3xl font-bold">{banner?.title}</h1>
+            <p className=" text-xs">{banner?.description}</p>
+            <button
+              type="button"
+              className=" bg-[#BDFD00] p-2 rounded-2xl text-black font-bold"
+            >
+              Get The Game
+            </button>
+          </div>
+          <div className="w-[55%] -translate-y-1 h-[115%]">
+            <img
+              src={"http://localhost:7000/" + banner?.image}
+              alt="baner-image"
+              className=" w-[90%] h-[90%]"
+            />
+          </div>
         </div>
-        <div className="w-[55%] -translate-y-1 h-[115%]">
-          <img src={"http://localhost:7000/"+banner?.image} alt="baner-image" className=" w-[90%] h-[90%]" />
-        </div>
+        <div className="w-[230px] h-[530px] bg-black rounded-l-2xl"></div>
       </div>
-      <div className="w-[230px] h-[530px] bg-black rounded-l-2xl">
-      
+      <div className="">
+        {cardDtyle}
       </div>
     </div>
   );
