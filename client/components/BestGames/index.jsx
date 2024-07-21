1b44cf0cd689eb20bd68e9c869e39863b006098a
Barbodmsd <barbodmasoudi@gmail.com>
2024-07-21 15:46:20 +0430

    end best games section

--- client/components/BestGames/index.jsx
+++ client/components/BestGames/index.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import ArrowIcon from "./ArrowIcon";
 
+// get data
 const getData = async () => {
   try {
     const res = await fetch(
@@ -12,17 +13,19 @@ const getData = async () => {
     console.log(error);
   }
 };
-export const BestGameCard = ({img,title,platform,id}) => {
+// create card in best games section
+export const BestGameCard = ({img,title,platform,id,price}) => {
   return (
     <Link href={ `/games/product-details/${id}/${title.replaceAll(" ", "-").toLowerCase()}`}>
-    <div className='w-[100%] items-center h-16 rounded-md flex justify-between pl-1 '>
+    <div className=' duration-300 hover:bg-my-yellow hover:-translate-x-5 w-[100%] items-center h-16 rounded-md flex justify-between  '>
       <div className='flex items-center gap-3 w-[60%] h-[100%]'>
         <img src={img} alt={title} className=' object-cover w-[50%] h-[100%] rounded-md' />
         <div className='flex flex-col gap-1'>
-          <h2 className='font-bold text-white text-sm'>{title.slice(0,4)}</h2>
-          <h6 className='text-txt text-xs'>{platform.slice(0,4)}</h6>
+          <h2 className=' font-bold text-white text-sm'>{title.slice(0,4)}</h2>
+          <h6 className='text-txt font-bold text-xs'>{platform.slice(0,4)}</h6>
         </div>
       </div>
+      <p className="text-black text-sm mr-2 font-bold ">${price}</p>
       <span className="text-txt px-2 rounded-full bg-bg-300"><ArrowIcon/></span>
     </div>
     </Link>
@@ -30,9 +33,12 @@ export const BestGameCard = ({img,title,platform,id}) => {
 };
 export default async function BestGames() {
   const products = await getData();
+
+  // import data into card
   const items = products?.map((e, index) => (
     <BestGameCard
       key={index}
+      price={e.price}
       id={e._id}
       img={"http://localhost:7000/" + e.images[1]}
       title={e.title}
@@ -40,13 +46,12 @@ export default async function BestGames() {
     />
   ));
   return (
-    <div className='flex flex-col p-2 py-5 gap-4 w-[200px] h-[500px] rounded-l-2xl bg-black'>
+    <section className='flex flex-col p-2 py-5 gap-4 w-[200px] h-[500px] rounded-l-2xl bg-black'>
       <span className="flex gap-4">
         <h5 className='font-bold'>Best Game</h5>
         <span className="px-2 font-bold rounded-full text-black bg-my-yellow">20</span>
       </span>
       {items}
-      
-    </div>
+    </section>
   );
 }
