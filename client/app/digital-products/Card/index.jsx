f1dc50af7ba39964a78cdfd2305b5c47410320de
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-07-22 05:37:52 -0700

    Update index.jsx

--- client/app/digital-products/Card/index.jsx
+++ client/app/digital-products/Card/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Tilt from "react-parallax-tilt";
 import "./style.css";
+import Link from "next/link";
 
-export default function index({title, brand,price,image}) {
+export default function index({ title, brand, price, image,id }) {
   return (
     <Tilt
       className="background-stripes rounded-2xl parallax-effect-glare-scale bg-[#28282A] w-[250px] h-[290px] "
@@ -12,42 +13,48 @@ export default function index({title, brand,price,image}) {
       glareMaxOpacity={1}
       scale={1}
     >
-      <div className="inner-element flex gap-10 flex-col">
-        <div className="flex flex-col justify-center items-center gap-2">
-          {/*text card*/}
-          <img src={image} alt="cart-image" className="w-[150px] -translate-y-10" />
-          <h1>{title}</h1>
-          <span>{brand}</span>
+      <Link href={`digital-products/product-details/${id}`}>
+        <div className="inner-element flex gap-10 flex-col">
+          <div className="flex flex-col justify-center items-center gap-2">
+            {/*text card*/}
+            <img
+              src={image}
+              alt="cart-image"
+              className="w-40 h-36 -translate-y-10"
+            />
+            <h1>{title}</h1>
+            <span>{brand}</span>
+          </div>
+          <div className="flex justify-between px-10">
+            <p className="praice">${price}</p>
+            <button type="button" className=" rounded-full hover:text-white">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M12 9V15"
+                  stroke="#BDFD00"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M9 12H15"
+                  stroke="#BDFD00"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <circle cx="12" cy="12" r="11.5" stroke="#BDFD00" />
+              </svg>
+            </button>
+          </div>
         </div>
-        <div className="flex justify-between px-10">
-          <p className="praice">${price}</p>
-          <button type="button" className=" rounded-full hover:text-white">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 9V15"
-                stroke="#BDFD00"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M9 12H15"
-                stroke="#BDFD00"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <circle cx="12" cy="12" r="11.5" stroke="#BDFD00" />
-            </svg>
-          </button>
-        </div>
-      </div>
+      </Link>
     </Tilt>
   );
 }
