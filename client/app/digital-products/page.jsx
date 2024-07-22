e7484f36380dca60159af81a8c4f061bbaa05498
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-07-22 07:27:12 -0700

    Update page.jsx

--- client/app/digital-products/page.jsx
+++ client/app/digital-products/page.jsx
@@ -4,11 +4,11 @@ import { useEffect, useState } from "react";
 import Card from "./Card";
 import Tilt from "react-parallax-tilt";
 import CardBest from "./CardBest";
+import Products from "./Productss"
 
 export default function DigitalProducts() {
   const [banner, setBanner] = useState();
   const [productsCard, setProductsCard] = useState();
-  const [activBtn, setActiveBtn] = useState("All");
   useEffect(() => {
     (async () => {
       try {
@@ -29,25 +29,6 @@ export default function DigitalProducts() {
       }
     })();
   }, []);
-
-  const createCard = (product, index) => (
-    <Card
-      id={product._id}
-      key={index}
-      title={product.title}
-      brand={product.brand}
-      price={product.price}
-      image={"http://localhost:7000/" + product.images[1]}
-    />
-  );
-
-  const filterProducts = (condition) =>
-    productsCard?.filter(condition).map(createCard);
-  const top = filterProducts((e) => e.top === true);
-  const popular = filterProducts((e) => e.popular === true);
-  const mostSuled = filterProducts((e) => e.mostSuled === true);
-  const cardDtyle = filterProducts((e) => e);
-
   const cardBest = productsCard?.map((e, index) => (
     <CardBest
       id={e?._id}
@@ -92,55 +73,7 @@ export default function DigitalProducts() {
           {cardBest}
         </div>
       </div>
-      <div className="flex mt-10 gap-20">
-        {/* text */}
-        <div>
-          <h5 className="font-bold text-xl">Our Games</h5>
-        </div>
-        {/* buttons */}
-        <div className="flex gap-5">
-          <button
-            type="button"
-            onClick={() => setActiveBtn("All")}
-            className={activBtn == "All" ? "btn-focus" : "btn-notFocus"}
-          >
-            All
-          </button>
-          <button
-            type="button"
-            onClick={() => setActiveBtn("Top")}
-            className={activBtn == "Top" ? "btn-focus" : "btn-notFocus"}
-          >
-            Top
-          </button>
-          <button
-            type="button"
-            onClick={() => setActiveBtn("Popular")}
-            className={activBtn == "Popular" ? "btn-focus" : "btn-notFocus"}
-          >
-            Popular
-          </button>
-          <button
-            type="button"
-            onClick={() => setActiveBtn("MostSold")}
-            className={activBtn == "MostSold" ? "btn-focus" : "btn-notFocus"}
-          >
-            Most Sold
-          </button>
-        </div>
-      </div>
-      <div className=" mt-16 mb-16 flex flex-wrap gap-20 ">
-        {activBtn === "Top"
-          ? top
-          : activBtn === "Popular"
-          ? popular
-          : activBtn === "MostSold"
-          ? mostSuled
-          : activBtn === "All"
-          ? cardDtyle
-          : ""}
-          <div className="h-40"></div>
-      </div>
+      <Products/>
     </div>
   );
 }
