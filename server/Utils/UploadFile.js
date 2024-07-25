525038534ee59ee70c876df58bb281b321fb7ef7
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-07-25 03:48:14 -0700

    Update UploadFile.js

--- server/Utils/UploadFile.js
+++ server/Utils/UploadFile.js
@@ -1,16 +1,16 @@
 import multer from "multer";
 import path from "path";
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "Public");
-    },
-    filename: (req, file, cb) => {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-    },
-  });
-  const upload = multer({
-    storage: storage,
-  });
 
-  export default upload
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "Public");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+export default upload;
