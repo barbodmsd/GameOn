ee29adcb0c680d5fef9f9e5da7c578e08494e9fc
Barbodmsd <barbodmasoudi@gmail.com>
2024-07-27 17:05:16 +0430

    end slider product details

--- client/app/digital-product-page/product-details/[...slugs]/slider/index.jsx
+++ client/app/digital-product-page/product-details/[...slugs]/slider/index.jsx
@@ -1,12 +1,14 @@
 "use client";
-import { Swiper, SwiperSlide } from "swiper/react";
+import DigitalProductCard from "@/components/DigitalProductCard";
+import { useEffect, useState } from "react";
 import "swiper/css";
 import "swiper/css/pagination";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "./slider.css";
-import { Pagination } from "swiper/modules";
-import { useEffect, useState } from "react";
-import fetchData from "@/Utils/FetchData";
-import DigitalProductCard from "@/components/DigitalProductCard";
+import Next from "./next";
+import Prev from "./prev";
+
 
 export default function Slider() {
   const [products, setProducts] = useState();
@@ -26,7 +28,7 @@ export default function Slider() {
     <SwiperSlide key={index}>
       <DigitalProductCard
         img={process.env.NEXT_PUBLIC_DB_HOST + e?.images[0]}
-        platform={e?.detailsGames[0]?.platform}
+        platform={e?.detailgames[0]?.platform}
         price={e?.price}
         title={e?.title}
         id={e._id}
@@ -35,7 +37,23 @@ export default function Slider() {
   ));
   return (
     <div div className='bg-bg-100 p-5 w-[1000px] my-10 h-[400px] rounded-2xl'>
-      <Swiper pagination={true} modules={[Pagination]} className='game-product'>
+      <div className='flex w-full justify-between'>
+        <h2 className='text-xl font-bold'>Our Games</h2>
+        <div className='flex gap-1 '>
+            <span className="prev p-2 border cursor-pointer text-my-yellow border-my-yellow rounded-full"><Prev/></span>
+            <span className="next p-2 border cursor-pointer text-my-yellow border-my-yellow rounded-full "><Next/></span>
+        </div>
+      </div>
+      <Swiper
+        navigation={{
+            nextEl:'.next',
+            prevEl:'.prev',
+        }}
+        loop={true}
+        slidesPerView={5}
+        pagination={true}
+        modules={[Navigation]}
+        className='game-product'>
         {items}
       </Swiper>
     </div>
