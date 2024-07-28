5972d7b0c19465e4528742977f3be6a4a9d4b191
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-07-28 14:57:04 -0700

    Update index.jsx

--- client/app/physical-product-page/PhysicalProductCard/index.jsx
+++ client/app/physical-product-page/PhysicalProductCard/index.jsx
@@ -11,7 +11,9 @@ export default function PhysicalProductCard({
   id,
 }) {
   return (
-    <Link href={`/physical-product-page/product-details/${id}`}>
+    <Link href={`/physical-product-page/product-details/${id}/${title
+        .replaceAll(" ", "-")
+        .toLowerCase()}`}>
       <Tilt
         className="background-stripes rounded-2xl parallax-effect-glare-scale bg-[#28282A] w-[206px] h-[303px] "
         perspective={4000}
