5fc1ccbe8da7f1c1d8835c38e33b93d4baaa595c
Barbodmsd <barbodmasoudi@gmail.com>
2024-07-28 15:44:03 +0430

    login form updated

--- client/app/auth/Login/index.jsx
+++ client/app/auth/Login/index.jsx
@@ -1,22 +1,35 @@
+import PasswordIcon from "@/components/icon/password";
+import UserIcon from "@/components/icon/user";
+import { DevTool } from "@hookform/devtools";
 import React from "react";
 import { useForm } from "react-hook-form";
 
 export default function Login() {
   const form = useForm();
-  const { register, handleSubmit, errors } = form;
-
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className='w-full h-full flex justify-evenly p-10 '>
       {/* form */}
       <div className='w-[350px] h-[400px] p-2 px-5 pt-10 rounded-xl flex flex-col items-center gap-2 bg-bg-100'>
-        <h5>LOGIN</h5>
-        <form noValidate className='flex w-full items-center p-1 flex-col gap-2'>
+        <h5 className='font-bold text-xl' style={{ letterSpacing: "2px" }}>
+          LOGIN
+        </h5>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+          className='flex w-full items-center p-1 flex-col gap-4'>
           {/* username */}
-          <div className=" w-full flex flex-col gap-2 text-left">
-            <label className="" htmlFor='username'>Username</label>
-            <div className="w-full relative">
+          <div className=' w-full flex flex-col gap-2 text-left'>
+            <label className='' htmlFor='username'>
+              Username
+            </label>
+            <div className='w-full relative'>
               <input
-              className="w-full pl-8 py-2 px-4 rounded-xl bg-bg-200"
+                className='w-full pl-8 py-2 px-4 rounded-xl bg-bg-200 outline-none'
                 type='text'
                 id='username'
                 placeholder='Username'
@@ -24,12 +37,48 @@ export default function Login() {
                   required: "username is required",
                 })}
               />
-              <span className="absolute left-[4%] top-[50%] translate-y-[-50%] ">m</span>
+              <span className='absolute left-[3%] top-[50%] translate-y-[-50%] text-txt  '>
+                <UserIcon />
+              </span>
             </div>
+            <p className='text-sm text-red-500 text-left'>
+              {errors.username?.message}
+            </p>
           </div>
+          {/* password */}
+          <div className=' w-full flex flex-col gap-2 text-left'>
+            <label className='' htmlFor='password'>
+              Password
+            </label>
+            <div className='w-full relative'>
+              <input
+                className='w-full pl-8 py-2 px-4 rounded-xl bg-bg-200 outline-none'
+                type='password'
+                id='password'
+                placeholder='Password'
+                {...register("password", {
+                  required: "password is required",
+                  minLength: {
+                    value: 6,
+                    message: "password must be at latest 6 character",
+                  },
+                })}
+              />
+              <span className='absolute left-[3%] top-[50%] translate-y-[-50%] text-txt  text-xs'>
+                <PasswordIcon className='text-xs' />
+              </span>
+            </div>
+            <p className='text-sm text-red-500 text-left'>
+              {errors.password?.message}
+            </p>
+          </div>
+          <button className=' bg-my-yellow py-2 px-16 text-black font-bold rounded-xl outline-none border-none'>
+            Login
+          </button>
         </form>
       </div>
       <div className='w-[50%] '></div>
+      <DevTool control={control} />
     </div>
   );
 }
--- client/components/icon/password/index.jsx
+++ client/components/icon/password/index.jsx
@@ -5,12 +5,12 @@ export default function PasswordIcon() {
     <>
       <svg
         xmlns='http://www.w3.org/2000/svg'
-        viewBox='0 0 24 24'
+        viewBox='0 0 20 20'
         fill='currentColor'
-        className='size-6'>
+        className='size-5'>
         <path
           fillRule='evenodd'
-          d='M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z'
+          d='M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z'
           clipRule='evenodd'
         />
       </svg>
