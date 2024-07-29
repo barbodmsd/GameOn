9f93ad92a02aa3088d1cd18a226c57a60a17c794
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-07-29 14:04:30 -0700

    Update page.js

--- client/app/page.js
+++ client/app/page.js
@@ -1,24 +1,39 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import fetchData from "@/Utils/FetchData";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode, Pagination } from "swiper/modules";
+import { FreeMode, Pagination, Autoplay } from "swiper/modules";
+import { motion, useInView } from "framer-motion";
 import ProductsList from "@/components/ProductsList";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
+import PhysicalProductCard from "./physical-product-page/PhysicalProductCard";
+import DigitalProductCard from "@/components/DigitalProductCard";
 
 export default function Home() {
+  const [products, setProducts] = useState();
+  const [productsG, setProductsG] = useState();
   const [imageSlider, setImageSlider] = useState([]); // Initialize as empty array
-
+  const [banner, setBanner] = useState();
+  const ref = useRef(null);
+  const inView = useInView(ref, { once: true });
+  const refTow = useRef(null);
+  const inViewTow = useInView(refTow, { once: true });
   // Fetch sliders data
   useEffect(() => {
     (async () => {
       try {
         const res = await fetchData("sliders");
+        const resBanner = await fetchData("banners");
+        const resProducts = await fetchData("products?key=physical");
+        const resProductsG = await fetchData("products?key=digital");
+        setProducts(resProducts.data);
+        setProductsG(resProductsG.data);
         setImageSlider(res.data);
+        setBanner(resBanner.data);
       } catch (error) {
         console.log(error);
       }
@@ -35,55 +50,153 @@ export default function Home() {
     </SwiperSlide>
   ));
 
+  const banerImage = banner?.map((e, index) => (
+    <img
+      key={index}
+      src={process.env.NEXT_PUBLIC_DB_HOST + e.image}
+      className=" w-[499px]"
+    />
+  ));
+
+  const cards = products?.map((product, index) => (
+    <SwiperSlide key={index}>
+      <PhysicalProductCard
+        id={product._id}
+        title={product.title}
+        brand={product.brand}
+        price={product.price}
+        image={process.env.NEXT_PUBLIC_DB_HOST + product.images[0]}
+      />
+    </SwiperSlide>
+  ));
+  const cardsTow = productsG?.map((product, index) => (
+    <SwiperSlide key={index}>
+      <DigitalProductCard
+        id={product._id}
+        title={product.title}
+        brand={product.brand}
+        price={product.price}
+        img={process.env.NEXT_PUBLIC_DB_HOST + product.images[0]}
+      />
+    </SwiperSlide>
+  ));
   return (
     <div className="px-8 mt-5 w-[1229px]">
       <div className="w-[1229px]">
-        <Swiper className="w-full h-96" loop>
+        <Swiper
+          className="w-full h-96"
+          loop
+          spaceBetween={30}
+          autoplay={{
+            delay: 3500,
+          }}
+          modules={[Autoplay]}
+        >
           {sliders}
         </Swiper>
       </div>
-      <div className="w-[1229px]">
+      <div className="w-[1229px] py-20 ">
+        <h2 className="text-2xl font-bold">Products</h2>
         <Swiper
-          slidesPerView={3}
-          spaceBetween={30}
+          slidesPerView={5}
+          spaceBetween={20}
           freeMode={true}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[FreeMode, Pagination]}
+          modules={[FreeMode]}
           className=" w-full my-10"
         >
-          <ProductsList />
+          {cards}
         </Swiper>
       </div>
-      <div className="h-56"> 2 baner</div>
-      <div className="w-[1229px]">
+      <div ref={ref} className=""></div>
+      {inView && (
+        <div className="show-dev flex justify-center gap-10 w-[1229px] py-10  ">
+          <motion.div
+            initial={{ opacity: 0, x: -500 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            {banerImage ? banerImage[4] : ""}
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: 500 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            {banerImage ? banerImage[5] : ""}
+          </motion.div>
+        </div>
+      )}
+      <div className="w-[1229px] my-20 ">
+        <h2 className="text-2xl font-bold">Products</h2>
         <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        freeMode={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[FreeMode, Pagination]}
-        className=" w-full my-10"
+          slidesPerView={5}
+          spaceBetween={20}
+          freeMode={true}
+          modules={[FreeMode]}
+          className=" w-full my-10"
         >
-          <ProductsList/>
+          {cardsTow}
         </Swiper>
       </div>
-      <div className="h-56"> boxs</div>
-      <div className="w-[1229px]">
+      <dev ref={refTow}></dev>
+      {inViewTow && (
+        <div className="flex justify-center gap-3">
+          <motion.div
+            initial={{ opacity: 0, y: 500 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            className="w-[268px] h-[375px] rounded-2xl bg-bg-300"
+          >
+            box1
+          </motion.div>
+          <div className="flex flex-col gap-4">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.8 }}
+              className="w-[493px] h-[148px] rounded-2xl bg-bg-300"
+            >
+              box top
+            </motion.div>
+            <div className="flex justify-center gap-3">
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.8 }}
+                className="w-[237px] h-[210px] rounded-2xl bg-bg-300"
+              >
+                L
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.8 }}
+                className="w-[237px] h-[210px] rounded-2xl bg-bg-300"
+              >
+                R
+              </motion.div>
+            </div>
+          </div>
+          <motion.div
+            initial={{ opacity: 0, y: -500 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            className="w-[268px] h-[375px] rounded-2xl bg-bg-300"
+          >
+            box2
+          </motion.div>
+        </div>
+      )}
+      <div className="w-[1229px] my-20 ">
+        <h2 className="text-2xl font-bold">Products</h2>
         <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        freeMode={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[FreeMode, Pagination]}
-        className=" w-full my-10"
+          slidesPerView={5}
+          spaceBetween={20}
+          freeMode={true}
+          modules={[FreeMode]}
+          className=" w-full my-10"
         >
-          <ProductsList/>
+          {cardsTow}
         </Swiper>
       </div>
     </div>
