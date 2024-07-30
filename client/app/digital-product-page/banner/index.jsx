a539c36b6c655a54807081d1fdbbde2076244a16
Barbodmsd <barbodmasoudi@gmail.com>
2024-07-30 22:08:29 +0430

    update banner

--- client/app/digital-product-page/banner/index.jsx
+++ client/app/digital-product-page/banner/index.jsx
@@ -25,18 +25,41 @@ export default function GameBanner() {
             {/* image */}
             <img
               src={process.env.NEXT_PUBLIC_DB_HOST + banners[2]?.image}
-              alt={banners[0]?.title}
+              alt={banners[2]?.title}
               className='w-[100%] scale-110  object-cover rounded-2xl -translate-y-2'
             />
             {/* text */}
             <div className='w-full flex flex-col gap-3 items-start p-5'>
               <h2 className='text-2xl  font-bold'>{banners[2]?.title}</h2>
-              <h6 className='text-xs '>{banners[2].description.slice(0,100)}</h6>
-              <button className="btn-focus border-none hover:scale-105 duration-300">GET THE GAME</button>
+              <h6 className='text-xs '>
+                {banners[2].description.slice(0, 100)}
+              </h6>
+              <button className='btn-focus border-none hover:scale-105 duration-300'>
+                GET THE GAME
+              </button>
             </div>
           </div>
           {/* right side */}
-          <div className='w-[60%] flex rounded-2xl bg-bg-300 h-[100%]'></div>
+          <div className='w-[60%] flex rounded-2xl bg-bg-300 h-[100%]'>
+            <div className='flex  justify-center flex-col gap-5 pl-3 w-[50%]'>
+              <h2 className='text-2xl  font-bold'>{banners[3]?.title}</h2>
+              <h6 className='text-xs '>
+                {banners[3].description}
+              </h6>
+              <div>
+                <button className='btn-focus border-none hover:scale-105 duration-300'>
+                  GET THE GAME
+                </button>
+              </div>
+            </div>
+            <div className='w-[50%] h-[100%]'>
+              <img
+                src={process.env.NEXT_PUBLIC_DB_HOST + banners[3]?.image}
+                alt={banners[3]?.title}
+                className='w-[100%]  object-cover'
+              />
+            </div>
+          </div>
         </div>
       ) : (
         <p>loading...</p>
