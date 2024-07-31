723875c6975560a71979ee38f31065532d2a55ea
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-07-31 11:12:50 -0700

    Update page.jsx

--- client/app/auth/page.jsx
+++ client/app/auth/page.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import Login from "./Login";
 import Register from "./Register";
+import AuthLayout from "./layout";
 
 export default function Auth() {
   const [pageType, setPageType] = useState('login');
@@ -9,13 +10,16 @@ export default function Auth() {
     setPageType(pageType === "login" ? "register" : "login");
   };
   return (
-    <div className='min-h-screen flex w-full justify-center px-8 mt-5 '>
+    <AuthLayout>
+          <div className='min-h-screen flex w-full justify-center px-8 mt-5 '>
       {pageType === "login" ? (
         <Login handlePageType={handlePageType} />
       ) : (
         <Register handlePageType={handlePageType} />
       )}
     </div>
+    </AuthLayout>
+
   );
 }
 
