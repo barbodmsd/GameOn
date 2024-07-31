f4c085e89760d54576bd0cc6455c0eb081c4e245
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-07-31 11:32:07 -0700

    Create page.js

---
+++ client/app/profile/page.js
@@ -0,0 +1,13 @@
+import ProfileLayout from "./layout";
+import SimpleLayout from "./layout";
+import sidNav from "./sideNav";
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div className="min-h-screen px-8 mt-5 ">
+      <sidNav />
+      Profile
+    </div>
+  );
+}
