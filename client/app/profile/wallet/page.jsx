2828080cf79d6acaccc8d4f629c20390436d59c5
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-08-01 05:37:45 -0700

    Update page.jsx

--- client/app/profile/wallet/page.jsx
+++ client/app/profile/wallet/page.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+"use client"
+import React, { useState } from "react";
+import CardPurchases from "../CardPurchases";
 
 export default function page() {
+    const [cardPrice ,setCardPrice] = useState(0)
+    const handelPriceCard = (vlaue)=>{
+        if(cardPrice>0){
+            setCardPrice(0)
+        }
+        setCardPrice(vlaue)
+    }
   return (
-    <div>wallet</div>
-  )
+    <div className="mx-10">
+      <h1 className="text-my-yellow mt-5 font-bold text-2xl">Wallet</h1>
+      <div className=" main flex flex-col gap-5 bg-bg-300 w-full h-full my-8 p-5 rounded-3xl">
+        <div className="flex w-full gap-5">
+          <div className=" bg-bg-100 w-[50%] h-full p-5 flex flex-col gap-32 rounded-2xl ">
+            <div className="flex justify-between">
+              <div>
+                <span className="text-3xl font-bold ">$ 0</span>
+                <div className="flex gap-10">
+                  <span className="text-green-800 text-lg">$ 20 +</span>
+                  <span className="text-red-800 text-lg">$ 5 -</span>
+                </div>
+              </div>
+              <div>
+                <span className="text-lg">$</span>
+              </div>
+            </div>
+            <div className="flex justify-between">
+                <div className=" border-my-yellow border rounded-lg p-2"><span>$ {cardPrice}</span></div>
+                <button type="button" className="bg-my-yellow px-10 rounded-md text-bg-100 font-bold" >Add Wallet</button>
+            </div>
+          </div>
+          <div className="w-[50%] h-full flex flex-wrap gap-5 justify-center">
+            <button type="button" onClick={()=>handelPriceCard(50)} className="w-[180px] h-[120px] bg-bg-100 hover:border hover:border-my-yellow rounded-2xl text-2xl hover:text-my-yellow">$ 50</button>
+            <button type="button" onClick={()=>handelPriceCard(100)} className="w-[180px] h-[120px] bg-bg-100 hover:border hover:border-my-yellow rounded-2xl text-2xl hover:text-my-yellow">$ 100</button>
+            <button type="button" onClick={()=>handelPriceCard(150)} className="w-[180px] h-[120px] bg-bg-100 hover:border hover:border-my-yellow rounded-2xl text-2xl hover:text-my-yellow">$ 150</button>
+            <button type="button" onClick={()=>handelPriceCard(200)} className="w-[180px] h-[120px] bg-bg-100 hover:border hover:border-my-yellow rounded-2xl text-2xl hover:text-my-yellow">$ 200</button>
+          </div>
+        </div>
+        <div>
+          <div className=" w-full bg-bg-100 p-5 rounded-lg">
+            <h3>purchases</h3>
+            <div className="w-full flex flex-col gap-5 py-5">
+                <CardPurchases/>
+                <CardPurchases/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
