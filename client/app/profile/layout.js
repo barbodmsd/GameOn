3299e3d0a6a46f8c0d52c7b04d0dc0e0aca9b70a
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-08-01 03:04:22 -0700

    Create layout.js

---
+++ client/app/profile/layout.js
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import SidNavProfile from "./sideNav";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="flex">
+          <SidNavProfile/>
+          <div className="w-full">
+            <Header />
+            {children}
+            <Footer />
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
