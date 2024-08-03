73c8e15deda44adefdae6f0b600273f20f1cb7b3
Barbodmsd <barbodmasoudi@gmail.com>
2024-08-03 14:28:15 +0430

    update auth

--- server/Controllers/AuthCn.js
+++ server/Controllers/AuthCn.js
@@ -3,10 +3,24 @@ import jwt from "jsonwebtoken";
 import User from "../Models/userModel.js";
 import catchAsync from "../Utils/catchAsync.js";
 import HandleError from "../Utils/handleError.js";
-import {  sendAuthCode, verifyAuthCode } from "../Utils/smsHandler.js";
+import { sendAuthCode, verifyAuthCode } from "../Utils/smsHandler.js";
 
-export const register = catchAsync(async (req, res) => {
-  const { role, password, ...others } = req.body;
+// export const register = catchAsync(async (req, res) => {
+//   const { role, password, ...others } = req.body;
+//   const hashPassword = bcrypt.hashSync(password, 10);
+//   const newUser = await User.create({ ...others, password: hashPassword });
+//   const token = jwt.sign(
+//     { role: newUser.role, id: newUser._id },
+//     process.env.SECRET_KEY
+//   );
+//   return res.status(201).json({
+//     status: "success",
+//     message: "Register successfully",
+//     data:{user:newUser,token}
+//   });
+// });
+export const register = catchAsync(async (req, res, next) => {
+  const { password, ...others } = req.body;
   const hashPassword = bcrypt.hashSync(password, 10);
   const newUser = await User.create({ ...others, password: hashPassword });
   const token = jwt.sign(
@@ -16,10 +30,9 @@ export const register = catchAsync(async (req, res) => {
   return res.status(201).json({
     status: "success",
     message: "Register successfully",
-    data:{user:newUser,token}
+    data: { user: newUser, token },
   });
 });
-
 export const login = catchAsync(async (req, res, next) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
@@ -46,10 +59,10 @@ export const login = catchAsync(async (req, res, next) => {
 
 export const sendSms = catchAsync(async (req, res, next) => {
   const { phone } = req.body;
-  const user=await User.findOne({phone})
-    if(!user){
-        return next(new HandleError("user with this phone number not exist"));
-    }
+  const user = await User.findOne({ phone });
+  if (!user) {
+    return next(new HandleError("user with this phone number not exist"));
+  }
   const send = await sendAuthCode(phone);
   if (!send.success) {
     return next(new HandleError(send.message, 500));
@@ -60,22 +73,22 @@ export const sendSms = catchAsync(async (req, res, next) => {
 });
 
 export const verifyCode = catchAsync(async (req, res, next) => {
-    const {code,phone}=req.body
-    const user=await User.findOne({phone})
-    const verify=await verifyAuthCode(phone,code)
-    if(!verify.success){
-        return next(new HandleError('code incorrect', 400));
-    }
-    const token = jwt.sign(
-        { role: user.role, id: user._id },
-        process.env.SECRET_KEY
-      );
-    return res.status(200).json({
-        status: "success",
-        message: "Login successfully",
-        data: {
-          user,
-          token,
-        },
-      });
+  const { code, phone } = req.body;
+  const user = await User.findOne({ phone });
+  const verify = await verifyAuthCode(phone, code);
+  if (!verify.success) {
+    return next(new HandleError("code incorrect", 400));
+  }
+  const token = jwt.sign(
+    { role: user.role, id: user._id },
+    process.env.SECRET_KEY
+  );
+  return res.status(200).json({
+    status: "success",
+    message: "Login successfully",
+    data: {
+      user,
+      token,
+    },
+  });
 });
