544adcbac89d929cc27acd5509ad66e47f78b16b
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-08-09 15:04:32 -0700

    remove loading

--- client/app/page.js
+++ client/app/page.js
@@ -47,7 +47,7 @@ export default function Home() {
   const sliders = imageSlider.map((e, index) => (
     <SwiperSlide key={index}>
       <img
-        className='w-full h-96 rounded-xl'
+        className="w-full h-96 rounded-xl"
         src={process.env.NEXT_PUBLIC_DB_HOST + e.image}
       />
     </SwiperSlide>
@@ -57,7 +57,7 @@ export default function Home() {
     <img
       key={index}
       src={process.env.NEXT_PUBLIC_DB_HOST + e.image}
-      className=' w-[499px]'
+      className=" w-[499px]"
     />
   ));
 
@@ -85,122 +85,129 @@ export default function Home() {
   ));
   return (
     <>
-      {banner ? (
-        <div className='px-8 mt-5 w-[1229px]'>
-          <div className='w-[1229px]'>
-            <Swiper
-              className='w-full h-96'
-              loop={true}
-              spaceBetween={30}
-              autoplay={{
-                delay: 3500,
-              }}
-              modules={[Autoplay]}>
-              {sliders}
-            </Swiper>
+      <div className="px-8 mt-5 w-[1229px]">
+        <div className="w-[1229px]">
+          <Swiper
+            className="w-full h-96"
+            loop={true}
+            spaceBetween={30}
+            autoplay={{
+              delay: 3500,
+            }}
+            modules={[Autoplay]}
+          >
+            {sliders}
+          </Swiper>
+        </div>
+        <div className="w-[1229px] py-20 ">
+          <h2 className="text-2xl font-bold">Products</h2>
+          <Swiper
+            slidesPerView={5}
+            spaceBetween={20}
+            freeMode={true}
+            modules={[FreeMode]}
+            className=" w-full my-10"
+          >
+            {cards}
+          </Swiper>
+        </div>
+        <div ref={ref} className=""></div>
+        {inView && (
+          <div className="show-dev flex justify-center gap-10 w-[1229px] py-10  ">
+            <motion.div
+              initial={{ opacity: 0, x: -500 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8 }}
+            >
+              {banerImage ? banerImage[4] : ""}
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: 500 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8 }}
+            >
+              {banerImage ? banerImage[5] : ""}
+            </motion.div>
           </div>
-          <div className='w-[1229px] py-20 '>
-            <h2 className='text-2xl font-bold'>Products</h2>
-            <Swiper
-              slidesPerView={5}
-              spaceBetween={20}
-              freeMode={true}
-              modules={[FreeMode]}
-              className=' w-full my-10'>
-              {cards}
-            </Swiper>
-          </div>
-          <div ref={ref} className=''></div>
-          {inView && (
-            <div className='show-dev flex justify-center gap-10 w-[1229px] py-10  '>
+        )}
+        <div className="w-[1229px] my-20 ">
+          <h2 className="text-2xl font-bold">Products</h2>
+          <Swiper
+            slidesPerView={5}
+            spaceBetween={20}
+            freeMode={true}
+            modules={[FreeMode]}
+            className=" w-full my-10"
+          >
+            {cardsTow}
+          </Swiper>
+        </div>
+        <dev ref={refTow}></dev>
+        {inViewTow && (
+          <div className="flex justify-center gap-3">
+            <motion.div
+              initial={{ opacity: 0, y: 500 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8 }}
+              className="w-[268px] h-[375px] rounded-2xl "
+            >
+              {banerImage[6]}
+            </motion.div>
+            <div className="flex flex-col gap-4">
               <motion.div
-                initial={{ opacity: 0, x: -500 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.8 }}>
-                {banerImage ? banerImage[4] : ""}
-              </motion.div>
-              <motion.div
-                initial={{ opacity: 0, x: 500 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.8 }}>
-                {banerImage ? banerImage[5] : ""}
-              </motion.div>
-            </div>
-          )}
-          <div className='w-[1229px] my-20 '>
-            <h2 className='text-2xl font-bold'>Products</h2>
-            <Swiper
-              slidesPerView={5}
-              spaceBetween={20}
-              freeMode={true}
-              modules={[FreeMode]}
-              className=' w-full my-10'>
-              {cardsTow}
-            </Swiper>
-          </div>
-          <dev ref={refTow}></dev>
-          {inViewTow && (
-            <div className='flex justify-center gap-3'>
-              <motion.div
-                initial={{ opacity: 0, y: 500 }}
-                animate={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
                 transition={{ duration: 0.8 }}
-                className='w-[268px] h-[375px] rounded-2xl '>
-                {banerImage[6]}
+                className="w-[493px] h-[148px] rounded-2xl "
+              >
+                {banerImage[8]}
               </motion.div>
-              <div className='flex flex-col gap-4'>
+              <div className="flex justify-center gap-3">
                 <motion.div
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
                   transition={{ duration: 0.8 }}
-                  className='w-[493px] h-[148px] rounded-2xl '>
-                  {banerImage[8]}
+                  className="w-[237px] h-[210px] rounded-2xl bg-bg-300"
+                >
+                  {banerImage[10]}
                 </motion.div>
-                <div className='flex justify-center gap-3'>
-                  <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 0.8 }}
-                    className='w-[237px] h-[210px] rounded-2xl bg-bg-300'>
-                    {banerImage[10]}
-                  </motion.div>
-                  <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 0.8 }}
-                    className='w-[237px] h-[210px] rounded-2xl bg-bg-300'>
-                    {banerImage[9]}
-                  </motion.div>
-                </div>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.8 }}
+                  className="w-[237px] h-[210px] rounded-2xl bg-bg-300"
+                >
+                  {banerImage[9]}
+                </motion.div>
               </div>
-              <motion.div
-                initial={{ opacity: 0, y: -500 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.8 }}
-                className='w-[268px] h-[375px] rounded-2xl '>
-                {banerImage[7]}
-              </motion.div>
             </div>
-          )}
-          <div className='w-[1229px] my-20 '>
-            <h2 className='text-2xl font-bold'>Products</h2>
-            <Swiper
-              slidesPerView={5}
-              spaceBetween={20}
-              freeMode={true}
-              modules={[FreeMode]}
-              className=' w-full my-10'>
-              {cardsTow}
-            </Swiper>
+            <motion.div
+              initial={{ opacity: 0, y: -500 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8 }}
+              className="w-[268px] h-[375px] rounded-2xl "
+            >
+              {banerImage[7]}
+            </motion.div>
           </div>
+        )}
+        <div className="w-[1229px] my-20 ">
+          <h2 className="text-2xl font-bold">Products</h2>
+          <Swiper
+            slidesPerView={5}
+            spaceBetween={20}
+            freeMode={true}
+            modules={[FreeMode]}
+            className=" w-full my-10"
+          >
+            {cardsTow}
+          </Swiper>
         </div>
-      ) : (
-        <Loading />
-      )}
+      </div>
 
       {/* toast */}
       <ToastContainer
-        position='top-center'
+        position="top-center"
         autoClose={5000}
         hideProgressBar={false}
         newestOnTop={false}
@@ -209,7 +216,7 @@ export default function Home() {
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme='colored'
+        theme="colored"
       />
     </>
   );
